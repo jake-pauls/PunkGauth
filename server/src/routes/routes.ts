c6ed38200d24c6c9a7821e4509d44e4950fa0ba2
jacob-pauls <jakepauls99@gmail.com>
2021-04-03 17:26:43 -0700

    Refactored JWT changes for nft-server prep

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,7 +1,7 @@
+import "dotenv/config";
 import { Application, Request, Response } from "express";
-require('dotenv').config()
-const jwt = require('jsonwebtoken')
-const express = require('express')
+import * as jwt from "jsonwebtoken";
+
 const posts = [
     {
       username: 'Isaac',
@@ -15,21 +15,22 @@ const posts = [
   
 
 export class Routes {
-    public routes(app: Application) : void {
-        app.use(express.json())
+    public routes(app: Application) : void {        
         app.route('/pgapi/ping').get((req: Request, res: Response) => {
             res.status(200).send({
                 ping: "pong"
             })
         });
+
         app.route('/posts').get((req: Request, res: Response) => {
-            res.status(200).send(res.json(posts))
+            res.status(200).send(res.json(posts));
         });
+
         app.route('/login').post((req: Request, res: Response) => {
-              //Auth User
-            const username = req.body.username
-            const user = { name: username }
-            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+            //Auth User
+            const username = req.body.username;
+            const user = { name: username };
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET as string);
             res.json({ accessToken: accessToken })
         });
     }
