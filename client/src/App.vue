00f6abf15f9fdb67932863f5bfef68e9012478ca
jacob-pauls <jakepauls99@gmail.com>
2021-04-03 23:15:08 -0700

    Modified font family Poppins, and used google import

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-<div>
-<Home />
-</div>
+  <div>
+    <Home />
+  </div>
 </template>
 
 <script>
@@ -16,16 +16,13 @@ export default {
 </script>
 
 <style>
+/* Poppins */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+/* Digestive */
 @font-face {
   font-family: "Digestive";
   src: local("Digestive"),
    url(./fonts/Digestive.otf) format("truetype");
-   
-}
-@font-face {
-  font-family: "Poppins";
-  src: local("Poppins"),
-   url(./fonts/Poppins/Poppins-Regular.ttf) format("truetype");
-   
 }
 </style>
