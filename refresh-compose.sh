e4bf730875e24378d99e93c57fb577e67ec52ec1
jacob-pauls <jakepauls99@gmail.com>
2021-04-03 20:54:59 -0700

    Modified color value positions in refresh-compose.sh

--- refresh-compose.sh
+++ refresh-compose.sh
@@ -13,21 +13,21 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 
-echo -e "${YELLOW}/server: ${WHITE}Checking packages on the server..."
+echo -e "${YELLOW}/server: Checking packages on the server..."
 cd server
 yarn
 
-echo -e "${YELLOW}/server: ${WHITE}Rebuilding TypeScript..."
+echo -e "${YELLOW}/server: Rebuilding TypeScript..."
 yarn build
 
-echo "${GREEN}/client: ${WHITE}Checking packages on the client..."
+echo "${GREEN}/client: Checking packages on the client..."
 cd .. && cd client
 yarn
 
-echo "${BLUE}docker: ${WHITE}Cleaning dangling Docker images..."
+echo "${BLUE}docker: Cleaning dangling Docker images..."
 cd .. && docker rmi $(docker images -aq -f 'dangling=true')
 
-echo "${BLUE}docker: ${WHITE}Relaunching Docker compose..."
+echo "${BLUE}docker: Relaunching Docker compose..."
 docker-compose up
 
 echo "${GREEN}Refresh complete."
