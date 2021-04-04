34260a3d3fad0e85620b703c1b702659deb036a5
Isaac Allen <69442967+IsaacMAllen@users.noreply.github.com>
2021-04-03 20:00:06 -0400

    added jwt functionality

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,11 +1,37 @@
 import { Application, Request, Response } from "express";
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const express = require('express')
+const posts = [
+    {
+      username: 'Isaac',
+      pinnedLocation: 'somewhere.blockchain'
+    },
+    {
+      useername: 'suckMcSucky',
+      pinnedLocation: 'idk'
+    }
+  ]
+  
 
 export class Routes {
     public routes(app: Application) : void {
+        app.use(express.json())
         app.route('/').get((req: Request, res: Response) => {
             res.status(200).send({
                 ping: "pong"
             })
         });
+        app.route('/posts').get((req: Request, res: Response) => {
+            res.status(200).send(res.json(posts))
+        });
+        app.route('/login').post((req: Request, res: Response) => {
+              //Auth User
+            const username = req.body.username
+            const user = { name: username }
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+            res.json({ accessToken: accessToken })
+        });
     }
 }
+
