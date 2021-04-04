feac727d7462551e454f99a6d91dffcdc6434bf5
jacob-pauls <jakepauls99@gmail.com>
2021-04-03 18:19:00 -0700

    Switched frontend component framework to vue

--- client/src/components/Counter.js
+++
@@ -1,6 +0,0 @@
-import React, { useState } from "react";
-
-export const Counter = ({ children }) => {
-    const [count, setCount] = useState(0);
-    return <div>{ children({ count, setCount }) }</div>
-} 
